2e69087dccf274a3385583513634dc96796e095e
charly <charly-970715@hotmail.com>
2023-09-05 21:29:26 -0500

    Creando_Tablas

---
+++ Creando_las_tablas_en_SQLite.py
@@ -0,0 +1,50 @@
+CREATE TABLE Cliente (
+    id INTEGER PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    telefono INTEGER NOT NULL
+ );
+  
+CREATE TABLE Sucursal (
+    id INTEGER PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    direccion TEXT NOT NULL
+ );
+
+CREATE TABLE Orden (
+    id INTEGER PRIMARY KEY,
+    cliente_id INTEGER,
+    sucursal_id INTEGER,
+    fecha_de_orden DATE NOT NULL,
+    total INTEGER
+);
+
+CREATE TABLE Stock (
+    id INTEGER PRIMARY KEY,
+    sucursal_id INTEGER,
+    producto_id INTEGER,
+    cantidad INTEGER,
+    UNIQUE (sucursal_id, producto_id),
+    FOREIGN KEY (sucursal_id) REFERENCES Sucursal(id),
+    FOREIGN KEY (producto_id) REFERENCES Producto(id)
+);
+
+CREATE TABLE Producto (
+	id INTEGER PRIMARY KEY,
+	nombre TEXT NOT NULL,
+	marca TEXT NOT NULL,
+	precio_unitario INTEGER NOT NULL,
+	categoria_id INTEGER
+);
+
+CREATE TABLE Item(
+	id  INTEGER PRIMARY KEY,
+	cantidad INT NOT NULL,
+	monto_venta INTEGER NOT NULL,
+	orden_id INTEGER,
+	producto_id INTEGER
+	);
+
+CREATE TABLE Categoria(
+	id INTEGER PRIMARY KEY,
+	nombre TEXT NOT NULL
+);
